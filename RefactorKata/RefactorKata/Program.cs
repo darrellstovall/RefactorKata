df763500ba8dd6bd1733ab758b04bda231766f18
Darrell Stovall <darrell.stovall.trussville@gmail.com>
2017-10-11 08:19:05 -0500

    Added the missing IEnumerable line

--- RefactorKata/RefactorKata/Program.cs
+++ RefactorKata/RefactorKata/Program.cs
@@ -6,16 +6,18 @@ namespace RefactorKata
 {
     internal class Program
     {
-        private static void Main (string[] args)
+        private static void Main(string[] args)
         {
-
             var products = GetProducts();
 
             foreach (var product in products)
             {
                 Console.WriteLine("This product is called: " + product.Name);
             }
-            using var conn =
+        }
+        private static IEnumerable<Product> GetProducts()
+        { 
+        using var conn =
             var conn = new SqlConnection("Server=.;Database=myDataBase;User Id=myUsername;Password = myPassword;");
 
             var cmd = conn.CreateCommand();
